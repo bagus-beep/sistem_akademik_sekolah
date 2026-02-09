093d617cb83ffea6af4da8ee3e87225f471b9a64
bagus-beep <bagus@smahangtuah2sda.sch.id>
2026-02-09 19:58:24 +0700

    Update pageLoader.js

--- js/core/pageLoader.js
+++ js/core/pageLoader.js
@@ -1,17 +1,17 @@
 import { initDashboard } from '../pages/dashboard.js';
 import { initTeachers } from '../pages/teachers.js';
-import { initStudents } from '../pages/students.js';
-import { initClasses } from '../pages/classes.js';
-import { initLessons } from '../pages/lessons.js';
-import { initSchedule } from '../pages/lesson_schedule.js';
+// import { initStudents } from '../pages/students.js';
+// import { initClasses } from '../pages/classes.js';
+// import { initLessons } from '../pages/lessons.js';
+// import { initSchedule } from '../pages/lesson_schedule.js';
 
 const pages = {
   dashboard: initDashboard,
   teachers: initTeachers,
-  students: initStudents,
-  classes: initClasses,
-  lessons: initLessons,
-  lesson_schedule: initSchedule
+  // students: initStudents,
+  // classes: initClasses,
+  // lessons: initLessons,
+  // lesson_schedule: initSchedule
 };
 
 export function initPage(page) {
@@ -21,4 +21,3 @@ export function initPage(page) {
   }
   pages[page]();
 }
-
